9c5fad1456ff54ec135c2cee62d751fcbe056551
LAHuebner <lennart.huebner@kuleuven.be>
2025-09-23 16:33:27 +0200

    type cast weight matrix to be float
    avoiding type error with numba

--- RandomTilings/draw_hexagon.py
+++ RandomTilings/draw_hexagon.py
@@ -4,8 +4,10 @@ from RandomTilings.shuffling import shuffling
 from RandomTilings.draw_lozenges import draw_lozenges
 
 def draw_hexagon(n,w,a=1,b=1,c=1,edge=0,dpi=200,show_figure=True):
+    w = w.astype(float)
     W = weight_hexagon(n,w,a,b,c)
     C = algorithm_reduction_weight(W)
     M = shuffling(C)
     fig = draw_lozenges(n,M,a,b,c,edge,dpi,show_figure)
+
     return fig
