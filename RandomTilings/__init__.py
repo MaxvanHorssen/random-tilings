407e223842997b33c3561d39acbd53816ed8afa3
Max van Horssen <maxjvanhorssen@gmail.com>
2025-09-29 15:46:40 +0200

    Included log partition routine for the hexagon and the Aztec diamond

--- RandomTilings/__init__.py
+++ RandomTilings/__init__.py
@@ -2,3 +2,5 @@ from .draw_aztec import draw_aztec
 from .draw_hexagon import draw_hexagon
 from .draw_aztec_gap import draw_aztec_gap
 from .draw_hexagon_gap import draw_hexagon_gap
+from .log_partition_function_hexagon_gap import log_partition_function_hexagon_gap
+from .log_partition_function_aztec_gap import log_partition_function_aztec_gap
